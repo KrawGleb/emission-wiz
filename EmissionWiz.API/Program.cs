855a9af1000d93fd506c40f71ac73cc2a9f89acc
HlebKravcevitch <hkrautsevich@1stquad.com>
2023-12-02 13:55:05 +0300

    + Add client + Migrate to .NET 8

--- EmissionWiz.API/Program.cs
+++ EmissionWiz.API/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -36,6 +37,9 @@ builder.Services.AddScoped<ILowWindDangerousWindSpeedCalculationManager, LowWind
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -43,8 +47,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
 
 app.Run();
