5944022f12bffde4549bd78ee36644d63bb46692
HlebKravcevitch <hkrautsevich@1stquad.com>
2024-02-26 23:28:20 +0300

    + Add tiff basics

---
+++ EmissionWiz.Logic/Managers/GeoTiffManager.cs
@@ -0,0 +1,24 @@
+﻿namespace EmissionWiz.Logic.Managers;
+
+internal class GeoTiffManager : BaseManager
+{
+    public void BuildTiffFromFlatArray(decimal[] source)
+    {
+
+    }
+
+    private byte[] BuildByteArray(decimal[] source)
+    {
+        var halfOfSize = source.Length;
+        var size = halfOfSize * 2;
+        var result = new byte[size, size];
+
+        var max = source.Max();
+        var unifiedArr = source.Select(x => (byte)(x / max * 255));
+
+
+
+        return new byte[0];
+    }
+
+}
